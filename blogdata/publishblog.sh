70e15a95061d74f90ecbbbe5dd1ca7c196e0ed1d
qz757 <qq757qz@gmail.com>
2016-12-07 22:04:09 +0800

    auto publish,/home/qz757/qz757.github.com/blogdata/git配置.md,/home/qz757/qz757.github.com/blogdata/手机电脑协同效率工具.md

--- blogdata/publishblog.sh
+++ blogdata/publishblog.sh
@@ -1,20 +1,26 @@
-mdlist=`find /home/qz757/qz757.github.com/blogdata/ -name "*.md"`
+mdlist=`find ~/qz757.github.com/blogdata/ -name "*.md"`
 mds=""
 for md in $mdlist
 do
 mds=$mds,$md
 done
 echo $mds
-if [ mds == "" ]
+echo "$mds"|grep -q "md"
+if [ $? -eq 0 ]
 then
-echo "no need to publish"
-exit 0
-else
-echo "publish"
+echo "publish to blogdata"
+cp ~/qz757.github.com/blogdata/*.md ~/blogdata/
+cd ~/blogdata
+git add -A
+git commit -m "auto publish"$mds
+git push
+echo "publish to web"
 cd ~/qz757.github.com
 mv blogdata/*.md source/_posts/
 git add -A
-git commit -m "auto publish"mds
+git commit -m "auto publish"$mds
 git push origin hexo
+else
+echo "no need to publish"
 fi
 cd
